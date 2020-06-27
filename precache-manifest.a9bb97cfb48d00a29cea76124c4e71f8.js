d43d991d6e8547960bd84282fd48e1773016cc09
ly <ly@ly.com>
2020-06-27 19:07:30 +0800

    Updates

---
+++ precache-manifest.a9bb97cfb48d00a29cea76124c4e71f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f2f082fd2f020d4603bfb514c47951",
+    "url": "/react-static-page/index.html"
+  },
+  {
+    "revision": "70115aa4285bb7c94545",
+    "url": "/react-static-page/static/css/main.6f690392.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/react-static-page/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-static-page/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70115aa4285bb7c94545",
+    "url": "/react-static-page/static/js/main.a51c9162.chunk.js"
+  },
+  {
+    "revision": "18641111bd90a9d27524",
+    "url": "/react-static-page/static/js/runtime-main.2c00115d.js"
+  }
+]);
